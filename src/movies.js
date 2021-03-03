b5899262dd8a497ef6abc40da02663b2a0e77f78
massytak <75225037+massytak@users.noreply.github.com>
2021-03-03 18:09:00 +0100

    5 iteration

--- src/movies.js
+++ src/movies.js
@@ -1,14 +1,13 @@
 // Iteration 1: All directors? - Get the array of all directors.
-  
 
-  function getAllDirectors(movies){
-      let directors= movies.map (function(movie){
-        return  movie.director;
-      })  
-      return directors    //[`peter weir,.....`]
-  }
-  
-  /*function getAllDirectors(movies){
+function getAllDirectors(movies) {
+  let directors = movies.map(function (movie) {
+    return movie.director;
+  });
+  return directors; //[`peter weir,.....`]
+}
+
+/*function getAllDirectors(movies){
     return movies.map (function(movie){
       return  movie.director;
     })  
@@ -37,33 +36,59 @@ function howManyMovies(moviesList){
 /v/f
 */
 
-function howManyMovies(moviesList){
-    // On filtre les films de Steven S 
-    let stevenMovies= moviesList.filter(function(el) {
-      return el.director==="Steven Spielberg"
-    }) // [ {...}, {...}, {. ]
-    // On filtre les dramas (de Steven S)
-    let stevenDramaMovies = stevenMovies.filter(function(el){
-      return el.genre.includes('Drama') // ture/false
-    })
-    return stevenDramaMovies.length ;
-  };
->>>>>>> 2c200917de06001a3248bc07805a73612612f68e
+function howManyMovies(moviesList) {
+  // On filtre les films de Steven S
+  let stevenMovies = moviesList.filter(function (el) {
+    return el.director === "Steven Spielberg";
+  }); // [ {...}, {...}, {. ]
+  // On filtre les dramas (de Steven S)
+  let stevenDramaMovies = stevenMovies.filter(function (el) {
+    return el.genre.includes("Drama"); // ture/false
+  });
+  return stevenDramaMovies.length;
+}
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
-function ratesAverage (movies){
-    if (movies.length<1){
-        return 0
-    }
-    let sum=movies.reduce (function(acc , val){
-        let filmRate= +(val.rate)||0;
-        return acc + filmRate
-    },0)
-return +(sum/movies.length).toFixed(2);
+function ratesAverage(movies) {
+  if (movies.length < 1) {
+    return 0;
+  }
+  let sum = movies.reduce(function (acc, val) {
+    let filmRate = +val.rate || 0;
+    return acc + filmRate;
+  }, 0);
+  return +(sum / movies.length).toFixed(2);
 }
 // Iteration 4: Drama movies - Get the average of Drama Movies
+//..
+function dramaMoviesRate(someMovies) {
+  let drMovies = someMovies.filter((eachMovie) =>
+    eachMovie.genre.includes("Drama")
+  );
+  return ratesAverage(drMovies);
+}
 
+//resultat la moyen  des filmm de drama
+//..
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-
+function orderByYear(obje) {
+  const arr = obje;
+  console.log(arr);
+  arr.sort(function (a, b) {
+    if (a.year > b.year) {
+      return 1;
+    } else if (a.year < b.year) {
+      return -1;
+    } else {
+      if (a.title > b.title) {
+        return 1;
+      } else a.title < b.title;
+      return -1;
+    }
+    return 0;
+  });
+  return arr;
+}
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
